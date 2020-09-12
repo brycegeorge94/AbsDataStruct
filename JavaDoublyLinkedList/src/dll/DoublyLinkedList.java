7c3794d0ef33dca09b121ebae659107d99da9ae9
brycegeorge94 <brycegeorge94@gmail.com>
2020-09-12 17:04:23 +1000

    Updated Tests, removed tail node, implementing as circular linked list

--- JavaDoublyLinkedList/src/dll/DoublyLinkedList.java
+++ JavaDoublyLinkedList/src/dll/DoublyLinkedList.java
@@ -1,17 +1,35 @@
 package dll;
 
 public class DoublyLinkedList {
-	private Node head;
-	private Node tail;
+	private Node head; // Node at index 0
 	private int size;
 	
 	public DoublyLinkedList() {}
 	
 	public DoublyLinkedList(Object obj) {
-		add(obj);
+		addFront(obj);
 	}
 	
-	public void add(Object obj) {
+	public void addFront(Object obj) {
+		if (head == null) {
+			// List is empty, create new node
+			head = new Node(obj, null, null);
+		} else if (head.next == null) {
+			// List only has head, create second node and link list
+			Node temp = new Node(obj, head, head);
+			head.next = head.prev = temp;
+			head = temp;
+		} else {
+			//make new node, next = head, prev = head.prev
+			Node temp = new Node(obj, head, head.prev);
+			head.prev.next = temp;
+			head.prev = temp;
+			head = temp;
+		}
+		size++;
+	}
+	
+	public void addBack(Object obj) {
 		
 	}
 	
--- JavaDoublyLinkedList/src/test/DoublyLinkedListTest.java
+++ JavaDoublyLinkedList/src/test/DoublyLinkedListTest.java
@@ -17,25 +17,35 @@ class DoublyLinkedListTest {
 	}
 
 	@Test
-	void addAndGetTest() {
+	void addFrontAndGetFromSingleTest() {
 		int i = 1;
-		dll.add(i);
+		dll.addFront(i);
+		assertEquals(i, dll.getFrom(0), "Object i should be at index zero");
+		assertEquals(1, dll.size(), "Size should equal one");
+	}
+	
+	@Test
+	void addBackAndGetIndexSingleTest() {
+		int i = 1;
+		dll.addBack(i);
 		assertEquals(0, dll.getIndex(i), "Object i should be at index zero");
 		assertEquals(1, dll.size(), "Size should equal one");
 	}
 	
 	@Test
 	void addAtAndGetFromTest() {
+		dll.addFront(2);
+		dll.addFront(3);
 		int i = 1;
-		dll.addAt(0, i);
-		assertEquals(i, dll.getFrom(0), "Object i should be at index zero");
+		dll.addAt(1, i);
+		assertEquals(i, dll.getFrom(1), "Object i should be at index zero");
 		assertEquals(1, dll.size(), "Size should equal one");
 	}
 	
 	@Test
 	void removeTest() {
 		int i = 1;
-		dll.add(i);
+		dll.addFront(i);
 		assertEquals(true, dll.remove(i), "Object i should be removed from list");
 		assertEquals(false, dll.remove(i), "Object i should already be removed from list");
 	}
@@ -43,7 +53,7 @@ class DoublyLinkedListTest {
 	@Test
 	void removeFromTest() {
 		int i = 1;
-		dll.add(2);
+		dll.addFront(2);
 		dll.addAt(0, i);
 		dll.removeFrom(0);
 		assertEquals(2, dll.getFrom(0), "Object at index zero should be two");
@@ -52,7 +62,7 @@ class DoublyLinkedListTest {
 	@Test
 	void containsTest() {
 		int i = 1;
-		dll.add(i);
+		dll.addFront(i);
 		assertEquals(true, dll.contains(i), "Object i should exist in list");
 		int j = 2;
 		assertEquals(false, dll.contains(j), "Object j should not exist in list");
